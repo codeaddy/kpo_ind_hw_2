f5c5571876a48bc216958b10424f55207dd33df4
codeaddy <115558165+codeaddy@users.noreply.github.com>
2023-01-09 02:18:26 +0300

    Closed scanner in main

--- concatenator_hw2/src/main/java/org/concatenator/Main.java
+++ concatenator_hw2/src/main/java/org/concatenator/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.print("Enter rootpath: ");
         Scanner scanner = new Scanner(System.in);
         String rootPath = scanner.nextLine();
+        scanner.close();
         Concatenator c = new Concatenator(rootPath);
         System.out.println(c.collectResultString());
 //        System.out.println(System.getProperty("user.dir"));
